6ee756db36b0fa3c9ee2d8eff6c3da83a91185c3
tiagopontes <49887066+tiagopontes@users.noreply.github.com>
2019-05-03 11:18:43 -0300

    Example Working
    Class examplo working

---
+++ NBCC-WMAD-RWProps2/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NBCC_WMAD_Console
+{
+    public class Car
+    {
+        private int? speed;
+        private decimal? price2;
+        private Nullable<decimal> price;
+        private int numberOfDoors;
+        private string carType;
+
+        public string CarType
+        {
+            get { return carType; }
+            set { carType = value; }
+        }
+
+        public int? Year { get; set; }
+
+        public int? Speed
+        {
+            get { return speed;}
+            set { speed = value;}
+        }
+
+        public Nullable<decimal> Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public int NumberOfDoors
+        {
+            get
+            {
+                return numberOfDoors;
+            }
+
+            set
+            {
+                if (value < 10)
+                {
+                    numberOfDoors = value;
+                }
+            }
+        }
+    }
+}
--- NBCC-WMAD-RWProps2/Program.cs
+++ NBCC-WMAD-RWProps2/Program.cs
@@ -14,8 +14,10 @@ namespace NBCC_WMAD_Console
         /// </summary>
         private static void LoadMenu()
         {
+            
             Menu.menuDictionary.Add(0, "Exit");
-            Menu.menuDictionary.Add(1, "About the Console");            
+            Menu.menuDictionary.Add(1, "About the Console");
+            Menu.menuDictionary.Add(2, "Car Builder");
         }
 
         static void Main(string[] args)
@@ -33,13 +35,66 @@ namespace NBCC_WMAD_Console
                         AboutThisApp();
                         break;
                     //Add more options in the menu here
+                    case 2:
+                        CarBuilder();
+                        break;
                 }
             }
 
             Out.P("Press any key to exit.");
             Console.ReadLine();
         }
+
+        // I want to store the cars I create
+        static List<Car> myCars = new List<Car>();
+
+        private static void CarBuilder()
+        {
+            string userCarType = In.GetString("Provide a car type (ie Car, Truck, Van):\n");
+            int userNumberOfDoors = In.GetInt("Provide the number of doors: ");
+            int userCarSpeed = In.GetInt("Provide the car speed: ");
+
+            Car car = new Car();
+
+            car.CarType = userCarType;
+            car.NumberOfDoors = userNumberOfDoors;
+            car.Speed = userCarSpeed;
+
+            Out.P("**************");
+            Out.P($"The Car created is:\nType: { car.CarType}" +
+                $"\nNumber of doors: {car.NumberOfDoors}" +
+                $"\nSpeed: {car.Speed}" +
+                $"\nYear: {car.Year}" +
+                $"\nPrice: {car.Price.GetValueOrDefault()}");
+
+            Out.P("**************\n");
+
+            myCars.Add(car);
+
+            Out.P($"I have a car count in my list of:  {myCars.Count}");
+
+            string userType = In.GetString($"Do you want to list all cars? y/n\n");
+            Out.P("**************\n");
+
+            if (userType.ToLower() == "y")
+            {
                 
+                foreach (Car c in myCars)
+                {
+                    Out.P("**********************");
+                    Out.P($"Type: {c.CarType}" +
+                        $"Number Of Doors: {c.NumberOfDoors}" +
+                        $"Car Speed: {c.Speed}" +
+                        $"Car Year: {c.Year}" + 
+                        $"Car Price: {c.Price.GetValueOrDefault().ToString("c")}");
+                }
+                Out.P("**********************");
+
+            }
+                   
+
+        }
+
         #region [My Functionality]
 
         /*
